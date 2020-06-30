ea2de88b196a49b5ee2e21fa268baba8435faaa2
urawa72 <urawa72@gmail.com>
2020-07-01 00:30:29 +0900

    add abc136d python

--- atcorder/abc136/d/main.py
+++ atcorder/abc136/d/main.py
@@ -6,10 +6,17 @@ mod = 10**9+7
 
 s = input().rstrip()
 
+n = len(s)
+ans = [0] * n
+
 rn = ln = 0
-for i in range(len(s)):
+for i in range(n):
     if s[i] == 'R': rn += 1
     if s[i] == 'L': ln += 1
-    if s[i - 1] != s[i]:
+    if s[i] == 'L' and (i + 1 < n and s[i + 1] == 'R' or i == n - 1):
+        ans[i - ln + 1] = ln - (ln // 2) + (rn // 2)
+        ans[i - ln] = rn - (rn // 2) + (ln // 2)
+        rn = ln = 0
 
-
+for i in ans:
+    print(i)
