2661ae30b7e9c53a8caf15258059f5a78f91eca7
urawa72 <urawa72@gmail.com>
2020-09-13 11:33:02 +0900

    add rust abc171

--- atcorder/rs/abc171/src/bin/d.rs
+++ atcorder/rs/abc171/src/bin/d.rs
@@ -2,5 +2,26 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+        q: usize,
+        bc: [(usize, usize); q]
+    }
+
+    let mut mp = vec![0; 100001];
+    let mut sum = 0;
+    for i in a {
+        sum += i;
+        mp[i] += 1;
+    }
+
+    for (b, c) in bc {
+        let tmp = mp[b];
+        sum = sum - b * tmp + c * tmp;
+        println!("{}", sum);
+        mp[b] = 0;
+        mp[c] += tmp;
+    }
+
 }
--- atcorder/rs/abc171/src/bin/e.rs
+++ atcorder/rs/abc171/src/bin/e.rs
@@ -2,5 +2,14 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        arr: [usize; n]
+    }
+
+    let sum = arr.iter().fold(0, |acc, val| acc ^ val);
+    for a in arr {
+        println!("{}", sum ^ a);
+    }
+
 }
