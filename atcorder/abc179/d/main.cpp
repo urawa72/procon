a784f23383cf11303561a88abf83efb796af925d
urawa72 <urawa72@gmail.com>
2020-09-21 20:59:41 +0900

    solve 1 problem

--- atcorder/abc179/d/main.cpp
+++ atcorder/abc179/d/main.cpp
@@ -9,32 +9,23 @@ const ll MOD = 998244353;
 
 int main() {
     int n, k; cin >> n >> k;
-    set<int> se;
-    for(int i = 0; i < k; i++){
-        int l, r; cin >> l >> r;
-        for(int j = l; j <= r; j++){
-            se.insert(j);
-        }
-    }
+    V<P<int, int> > v(k);
+    for(int i = 0; i < k; i++) cin >> v[i].first >> v[i].second;
 
-    int mi = 200001;
-    for(auto ss : se) {
-        mi = min(mi, ss);
-    }
-
-    V<ll> dp(n + 1, 0);
-    dp[1] = 1;
-    for(int i = 1; i + mi <= n; i++){
-        for(auto s : se){
-            if(i + s <= n) {
-                dp[i + s] += dp[i];
-                dp[i + s] %= MOD;
-            }else{
-                break;
-            }
+    // dpを累積和で高速化
+    V<ll> dp(n, 0), s(n + 1, 0);
+    dp[0] = 1, s[1] = 1;
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j < k; j++){
+            int l = max(0, i - v[j].second);
+            int r = max(0, i - v[j].first + 1);
+            dp[i] += (s[r] - s[l] + MOD) % MOD;
+            dp[i] %= MOD;
         }
+        s[i + 1] = dp[i] + s[i];
+        s[i + 1] %= MOD;
     }
-    cout << dp[n] % MOD << endl;
+    cout << dp[n - 1] << endl;
 
     return 0;
 }
