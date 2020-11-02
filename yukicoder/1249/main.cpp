e693a6bc482fb9348156b81c59aa5ffbfd2f4079
urawa72 <urawa72@gmail.com>
2020-11-02 23:33:29 +0900

    solve 1 problem

---
+++ yukicoder/1249/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(v) v.begin(), v.end()
+#define V vector
+#define P pair
+using ll = long long;
+
+
+int main() {
+    int a, b, c; cin >> a >> b >> c;
+    if(a + b == c) cout << "Correct" << endl;
+    else cout << "Incorrect" << endl;
+
+    return 0;
+}
