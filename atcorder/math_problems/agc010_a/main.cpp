b3d9d40cd86295994c6a54f208fe49b14dbb48d5
urawa72 <urawa72@gmail.com>
2020-11-28 16:16:55 +0900

    add math problems

---
+++ atcorder/math_problems/agc010_a/main.cpp
@@ -0,0 +1,54 @@
+// include headers {{{
+#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iomanip>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+#define ALL(v) v.begin(), v.end()
+#define V vector
+#define P pair
+using ll = long long;
+// }}}
+
+
+int main() {
+    int n; cin >> n;
+    V<ll> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    ll sum = accumulate(a.begin(), a.end(), 0);
+    if(sum % 2 == 0) cout << "YES" << endl;
+    else cout << "NO" << endl;
+
+    return 0;
+}
