84ab1b21f9a714da71dfcd6c0c9ef10c911d68c9
urawa72 <urawa72@gmail.com>
2020-11-29 21:55:43 +0900

    add 1 math problem

---
+++ atcorder/math_problems/abc159_c/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+using namespace std;
+
+#define ALL(v) v.begin(), v.end()
+#define V vector
+#define P pair
+using ll = long long;
+
+
+int main() {
+    double l; cin >> l;
+    cout << setprecision(12) << (l / 3) * (l / 3) * (l / 3) << endl;
+
+    return 0;
+}
