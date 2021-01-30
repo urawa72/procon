1aa736e4da9896d960fbc78f44b98b8c3a13a7d9
urawa72 <urawa72@gmail.com>
2021-01-30 23:17:10 +0900

    solve 1 problem

--- atcorder/abc188/d/main.cpp
+++ atcorder/abc188/d/main.cpp
@@ -9,5 +9,28 @@ using ll = long long;
 
 
 int main() {
+    ll n, C; cin >> n >> C;
+
+    V<P<ll, ll> > v;
+    for(ll i = 0; i < n; i++){
+        ll a, b, c; cin >> a >> b >> c;
+        v.emplace_back(a - 1, c);
+        v.emplace_back(b, -c);
+    }
+
+    sort(ALL(v));
+
+    ll ans = 0, f = 0, t = 0;
+    for(auto p : v) {
+        ll x = p.first;
+        ll y = p.second;
+        if(x != t){
+            ans += min(C, f) * (x - t);
+            t = x;
+        }
+        f += y;
+    }
+
+    cout << ans << endl;
     return 0;
 }
