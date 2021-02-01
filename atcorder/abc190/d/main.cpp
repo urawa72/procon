461752c94e96b6d1b0e8125b5fd8bbceac719d2f
urawa72 <urawa72@gmail.com>
2021-02-01 22:57:34 +0900

    add abc190 d

--- atcorder/abc190/d/main.cpp
+++ atcorder/abc190/d/main.cpp
@@ -8,6 +8,22 @@ using ll = long long;
 
 
 int main() {
+    ll n; cin >> n;
+
+    // 等差数列の和
+    ll n2 = n * 2;
+    int ans = 0;
+    auto check = [&](ll l) {
+        ll tmp = n2 / l - l + 1;
+        if(abs(tmp) % 2 == 0) ans++;
+    };
+    for(ll x = 1; x * x <= n2; x++){
+        if(n2 % x) continue;
+        ll y = n2 / x;
+        check(x);
+        if(x != y) check(y);
+    }
+    cout << ans << endl;
 
     return 0;
 }
