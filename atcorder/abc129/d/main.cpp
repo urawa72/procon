bc5c2ed661cfc25c9310de158b5383c4a959085d
urawa72 <urawa72@gmail.com>
2021-04-25 21:50:56 +0900

    solve abc129 d

--- atcorder/abc129/d/main.cpp
+++ atcorder/abc129/d/main.cpp
@@ -14,6 +14,51 @@ int main() {
     cin >> s[i];
   }
 
+  V<V<int>> vh(h + 1, V<int>(w + 1));
+  V<V<int>> vw(h + 1, V<int>(w + 1));
+  for (int j = 0; j < w; j++) {
+    for (int i = 0; i < h; i++) {
+      if(s[i][j] == '#'){
+        continue;
+      }else {
+        int c = 0, st = i;
+        while(1){
+          if(i == h || s[i][j] == '#') break;
+          c++;
+          i++;
+        }
+        for (int l = st; l <= i; l++) {
+          vh[l][j] = c;
+        }
+      }
+    }
+  }
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      if(s[i][j] == '#'){
+        continue;
+      }else{
+        int c = 0, st = j;
+        while(1){
+          if(j == w || s[i][j] == '#') break;
+          c++;
+          j++;
+        }
+        for (int l = st; l <= j; l++) {
+          vw[i][l] = c;
+        }
+      }
+    }
+  }
+
+  int ans = 0;
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      ans = max(ans, vh[i][j] + vw[i][j] - 1);
+    }
+  }
+  cout << ans << endl;
+
 
   return 0;
 }
