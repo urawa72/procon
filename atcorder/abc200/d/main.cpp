55e2bbd3c314f058d569fc79ffdc5eee63c3e4e2
urawa72 <urawa72@gmail.com>
2021-05-10 08:58:40 +0900

    solve abc200 d

--- atcorder/abc200/d/main.cpp
+++ atcorder/abc200/d/main.cpp
@@ -6,9 +6,40 @@ using namespace std;
 #define P pair
 using ll = long long;
 
-
 int main() {
-  
+  int n;
+  cin >> n;
+  V<int> a(n);
+  for (auto &x : a) cin >> x;
+  map<int, V<int>> mp;
+  auto output = [](V<int> &w){
+    cout << w.size();
+    for(auto &x : w) cout << ' ' << x;
+    cout << '\n';
+  };
+
+  int cnt = min(n, 8);
+  for (int bit = 0; bit < (1 << cnt); bit++) {
+    int sum = 0;
+    V<int> v;
+    for (int j = 0; j < cnt; j++) {
+      if (bit & 1 << j) {
+        v.push_back(j + 1);
+        sum += a[j];
+        sum %= 200;
+      }
+    }
+    if (mp[sum].size() != 0) {
+      cout << "Yes" << endl;
+      output(mp[sum]);
+      output(v);
+      return 0;
+    }else{
+      mp[sum] = v;
+    }
+  }
+
+  cout << "No" << endl;
 
   return 0;
 }
